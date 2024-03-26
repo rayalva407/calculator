716fb395c9ce91103e4c0dba641be9a4cd42737b
rayalva407 <rayalva407@gmail.com>
2024-03-26 11:33:54 -0600

    feat: adds functions for basic math operation (add, subtract, multiply, divide)

---
+++ script.js
@@ -0,0 +1,18 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "Division by zero error. Try again!";
+  }
+  return num1 / num2;
+}
