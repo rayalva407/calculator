c403e8a5fc90b68533b748d44f9d9e152fd27db8
rayalva407 <rayalva407@gmail.com>
2024-03-26 13:13:09 -0600

    feat: creates operate function

--- script.js
+++ script.js
@@ -17,6 +17,25 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
+function operate(num1, num2, operation) {
+  switch (operation) {
+    case "+":
+      add(num1, num2)
+      break;
+    case "-":
+      subtract(num1, num2)
+    case "*":
+      multiply(num1, num2)
+    case "/":
+      divide(num1, num2)
+    default:
+      break;
+  }
+
+
+  
+}
+
 let num1 = 0;
 let num2 = 0;
 let op = "";
