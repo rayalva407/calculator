3b910ee3c4bdc3962bfdd0c7d7301cce5a1f26db
rayalva407 <rayalva407@gmail.com>
2024-03-27 19:27:24 -0600

    feat: allows for key presses to change display

--- script.js
+++ script.js
@@ -18,16 +18,17 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operation) {
+  console.log(operation == "+")
+  
   switch (operation) {
     case "+":
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case "-":
-      subtract(num1, num2);
+      return subtract(num1, num2);
     case "*":
-      multiply(num1, num2);
+      return multiply(num1, num2);
     case "/":
-      divide(num1, num2);
+      return divide(num1, num2);
     default:
       break;
   }
@@ -36,6 +37,7 @@ function operate(num1, num2, operation) {
 let num1 = 0;
 let num2 = 0;
 let op = "";
+let numSwitch = true;
 const displayText = document.querySelector(".display-text");
 let displayString = displayText.innerText;
 const keys = document.querySelectorAll(".key");
@@ -43,13 +45,35 @@ const keys = document.querySelectorAll(".key");
 keys.forEach((key) => {
   key.addEventListener("click", () => {
     const value = key.innerText;
+    displayString = displayString + value;
+    if (displayString[0] === "0") {
+      displayString = displayString.slice(1)
+    }
 
-    if (displayString === "0") {
-      displayString = value;
-    }
-    else {
-      displayString += key.innerText;
-    }
+    // if (value === "+" || value === "-" || value === "*" || value === "/") {
+
+    //   op = value
+
+    //   if (numSwitch === false) {
+    //     num2 = Number(displayString);
+    //     displayString = operate(num1, num2, op);
+    //     typeof displayString
+    //     num1 = displayString
+    //   }
+    //   else {
+    //     num1 = Number(displayString)
+    //     displayString = "0";
+    //   }
+    //   numSwitch = !numSwitch;
+    // }
+    // else {
+    //   if (displayString[0] === "0") {
+    //     displayString = value
+    //   }
+    //   else {
+    //     displayString += value;
+    //   }
+    // }
 
     displayText.innerText = displayString;
   });
