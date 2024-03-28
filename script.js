c13218d0a4c28bf6dd4b9108059c47d015433adc
rayalva407 <rayalva407@gmail.com>
2024-03-28 13:43:59 -0600

    feat: calculator solves and displays simple calculations with two numbers

--- script.js
+++ script.js
@@ -18,10 +18,11 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operation) {
-  console.log(operation == "+")
   
   switch (operation) {
+    
     case "+":
+      console.log(num1);
       return add(num1, num2);
     case "-":
       return subtract(num1, num2);
@@ -34,10 +35,22 @@ function operate(num1, num2, operation) {
   }
 }
 
-let num1 = 0;
-let num2 = 0;
-let op = "";
-let numSwitch = true;
+function operationReady(op) {
+  for (k in op) {
+    if (op[k] === undefined) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const operation = {
+  num1: undefined,
+  operator: undefined,
+  num2: undefined
+}
+
 const displayText = document.querySelector(".display-text");
 let displayString = displayText.innerText;
 const keys = document.querySelectorAll(".key");
@@ -45,35 +58,35 @@ const keys = document.querySelectorAll(".key");
 keys.forEach((key) => {
   key.addEventListener("click", () => {
     const value = key.innerText;
-    displayString = displayString + value;
-    if (displayString[0] === "0") {
-      displayString = displayString.slice(1)
+
+    if (value === "+" || value === "-" || value === "*" || value === "/") {
+      operation.operator = value
+
+      if (operation.num1 === undefined) {
+        operation.num1 = Number(displayString)
+      }
+      else{
+        operation.num2 = Number(displayString)
+      }
+
+      console.log(operationReady(operation))
+      
+      if (operationReady(operation)) {
+        displayString = operate(operation.num1, operation.num2, operation.operator)
+      }
+
+      displayString = "0"
     }
-
-    // if (value === "+" || value === "-" || value === "*" || value === "/") {
-
-    //   op = value
-
-    //   if (numSwitch === false) {
-    //     num2 = Number(displayString);
-    //     displayString = operate(num1, num2, op);
-    //     typeof displayString
-    //     num1 = displayString
-    //   }
-    //   else {
-    //     num1 = Number(displayString)
-    //     displayString = "0";
-    //   }
-    //   numSwitch = !numSwitch;
-    // }
-    // else {
-    //   if (displayString[0] === "0") {
-    //     displayString = value
-    //   }
-    //   else {
-    //     displayString += value;
-    //   }
-    // }
+    else if (value === "=") {
+      operation.num2 = Number(displayString)
+      displayString = operate(operation.num1, operation.num2, operation.operator)
+    }
+    else {
+      displayString = displayString + value;
+      if (displayString[0] === "0") {
+        displayString = displayString.slice(1)
+      }
+    }
 
     displayText.innerText = displayString;
   });
